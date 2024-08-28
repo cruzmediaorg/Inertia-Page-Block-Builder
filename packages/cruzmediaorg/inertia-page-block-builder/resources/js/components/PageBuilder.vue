5eda4c560dae7bd9d0e63804fdbcb1c76c8ab5e2
Luis De la Cruz <98129640+cruzmediaorg@users.noreply.github.com>
2024-08-28 21:23:09 +0200

    wip

--- packages/cruzmediaorg/inertia-page-block-builder/resources/js/components/PageBuilder.vue
+++ packages/cruzmediaorg/inertia-page-block-builder/resources/js/components/PageBuilder.vue
@@ -49,23 +49,10 @@
               </svg>
             </button>
             <button @click="toggleFullScreen" class="bg-white p-2 border rounded-md">
-              <svg
-                class="w-6 h-6 text-gray-500"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8 4H4m0 0v4m0-4 5 5m7-5h4m0 0v4m0 4-5-5M8 20H4m0 0v-4m0 4-5-5"
-                />
-              </svg>
+              <svg class="w-6 h-6 text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 4H4m0 0v4m0-4 5 5m7-5h4m0 0v4m0-4-5 5M8 20H4m0 0v-4m0 4 5-5m7 5h4m0 0v-4m0 4-5-5"/>
+</svg>
+
             </button>
           </div>
         </div>
